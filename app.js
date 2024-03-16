59188dce50f73dfbaaacc62b2eb46e6dec98a225
Joshi <66480820+j05hi-00@users.noreply.github.com>
2024-03-16 14:00:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const form = document.querySelector('#create-account-form');
-const usernameInput = document.querySelector('#username');
+const usernameInput = document.querySelector('#username');//leni lingi leni lindi 
 const emailInput = document.querySelector('#email');
 const passwordInput = document.querySelector('#password');
 const confirmPasswordInput = document.querySelector('#confirm-password');
